4af55903ab11cf9576a290c2715ffb322dd1b44d
Anuvind Bhat <anuvind.bhat@gmail.com>
2018-05-28 13:43:15 +0530

    minor

--- tree.c
+++ tree.c
@@ -1,3 +1,10 @@
+/*
+AVL Tree in C
+Author: Snuvind Bhat
+Date: 28th May 2018
+A C implementation of a multi-set using an AVL Tree as the underlying data structure
+*/
+
 #include "tree.h"
 #include <stdlib.h>
 
